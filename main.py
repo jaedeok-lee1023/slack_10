9dc41639b5927f7b89579db25f044e1f66bd0d47
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2024-12-13 14:07:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,26 +18,26 @@ def send_slack_message(message, channel):
 def main():
     for cluster in clusters:
         # 메시지 제목 설정
-        header = f":loudspeaker: *『인사총무팀 공지』*\n\n"
+        header = f":loudspeaker: *『인사총무팀 공지』* <!channel>\n\n"
 
         notice_msg = (
-            f"안녕하세요? 평택 클러스터 구성원 여러분\n*평택 클러스터 출/퇴근 셔틀 이용 관련 안내사항 Remind* 공유 드립니다.\n"
-            f"*（전체공지_매주1회）*\n\n"
+            f"안녕하세요? 평택 클러스터 구성원 여러분\n*[신규입사자 개인 사물함 배정]* 공지 드립니다.\n\n"
             f"\n"
             f"\n"
-            f":체크1: *자세한 자료는 아래 첨부 파일 확인 바랍니다!*\n\n"
+            f":체크1: *12월9일 신규 입사자 사물함 배정을* 공지 드리오니 이용에 참고 부탁드립니다.\n\n"
             f"\n"
             f"\n"
-            f"*첨부:one: - <https://50072f98-e1d6-4b35-b1e5-5564ad1fcebf.usrfiles.com/ugd/50072f_535ab3cc9a594f068a41b6d49fa98872.xlsx|셔틀 노선도>*\n"
-            f"*첨부:two: - <https://50072f98-e1d6-4b35-b1e5-5564ad1fcebf.usrfiles.com/ugd/50072f_f1014320239a4796a138f0f43454f119.pptx|셔틀 노선 및 경유지 상세 이미지 포함>*\n"
-            f"*첨부:three: - <https://50072f98-e1d6-4b35-b1e5-5564ad1fcebf.usrfiles.com/ugd/50072f_1de4723281ae433f910a351a969f5280.pdf|셔틀 승,하차 장소>*\n"
-            f"*첨부:four: - <https://50072f98-e1d6-4b35-b1e5-5564ad1fcebf.usrfiles.com/ugd/50072f_b72245b692984e838e89f72711e2aea8.pdf|셔틀 어플 사용 안내>*\n"
-            f"*첨부:five: - <https://static.wixstatic.com/media/50072f_0b3f8852555b4c31b92834ce9f36f128~mv2.png|셔틀 （퇴근）출발 시간 안내>*\n\n"
+            f":ck11: *사물함 장소* 는 *6층* 에 있습니다.\n"
+            f":ck11: 사물함 배정 후 *자물쇠는 본인 지참* 부탁드립니다.\n"
+            f":ck11: 사물함 내부는 *CCTV 미 설치 구역*으로 자물쇠를 꼭 이용 바랍니다.\n"
+            f":ck11: *배정 받은 사물함 외 사용 불가* 하오니 꼭 배정받은 사물함을 사용 바랍니다.\n"
+            f":ck11: 사물함 배정은 순차적으로 진행 되며, *임의 변경* 은 불가 합니다.\n\n"
             f"\n"
             f"\n"
-            f"*자세한 자료는 :point_up: :point_up: 클릭하여 확인 부탁드립니다!*\n\n"
-            f"*문의사항 : 인사총무팀 총무/시설 담당자*\n\n"
+            f" 배정 받으신 사물함 번호를 모르실 경우 *인사총무팀 총무/시설 담당자* 에게 연락 바랍니다.\n\n"
+            f"\n"
             f"감사합니다.\n"
+            f"*첨부:one: - <https://docs.google.com/spreadsheets/d/1bS8lkrROJI9X0E2NaC8Z9ha9XXYBihwwOT5UzITxFF8/edit?usp=sharing|신규입사자 개인 사물함 배정>*\n"
         )
  
         # 메시지 본문
