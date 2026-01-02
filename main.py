808f15f13b56ffe8afc23c9df9f7ffeeda5b23b4
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2026-01-02 14:35:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,28 +41,24 @@ def send_slack_message(message, channel):
 def main():
     for cluster in clusters:
         # 메시지 제목 설정
-        header = f":loudspeaker: *『인사총무팀 공지』* <!channel>\n\n"
+        header = f"*[공지｜신규입사자 사물함 배정 안]*\n\n\n"
 
         notice_msg = (
-            f"안녕하세요? 평택 클러스터 구성원 여러분!\n"
+            f"1. *중요도* : 하\n"
+            f"2. *대상* : 평택 클러스터 신규 입사자\n"
+            f"3. *주요 내용*\n\n"
             f"\n"
+            f"안녕하세요? 우리 클러스터 신규 입사자 여러분!\n"
+            f"*사물함 배정* 되어 안내 드리오니 이용에 참고 부탁드립니다. 😊\n\n"
             f"\n"
-            f":white_check_mark: *금일 신규 입사자 사물함 배정을* 공지 드리오니 이용에 참고 부탁드립니다. 😊\n\n"
-            f"\n"
-            f"\n"
-            f":ck11: *사물함 장소* 는 *6층* 에 있습니다.\n"
+            f":ck11: *<https://docs.google.com/spreadsheets/d/1bS8lkrROJI9X0E2NaC8Z9ha9XXYBihwwOT5UzITxFF8/edit?usp=sharing|사물함 배정 및 장소>* 는 *6층* 에 있습니다.\n"
             f":ck11: 사물함 배정 후 *자물쇠는 본인 지참* 부탁드립니다.\n"
             f":ck11: 사물함 내부는 *CCTV 미 설치 구역* 으로 자물쇠를 꼭 이용 바랍니다.\n"
             f":ck11: *배정 받은 사물함 외 사용 불가* 하오니 꼭 배정받은 사물함을 사용 바랍니다.\n"
             f":ck11: 사물함 배정은 순차적으로 진행 되며, *임의 변경* 은 불가 합니다.\n\n"
             f"\n"
-            f"\n"
-            f" 배정 받으신 사물함 번호를 모르실 경우 *인사총무팀 총무/시설 담당자* 에게 연락 바랍니다.\n\n"
-            f"\n"
-            f"감사합니다.\n\n"
-            f"\n"
-            f"\n"
-            f" *(Click) (Click) :point_right: <https://docs.google.com/spreadsheets/d/1bS8lkrROJI9X0E2NaC8Z9ha9XXYBihwwOT5UzITxFF8/edit?usp=sharing|신규 입사자 개인 사물함 배정 현황>*\n"
+            f"*:slack: 문의사항 : 인사총무팀 총무/시설 담당자*\n\n"
+            f"감사합니다.\n"
         )
  
 # 메시지 본문
